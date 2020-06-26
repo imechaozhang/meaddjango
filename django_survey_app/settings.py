0513f896576f2dd4d7546f23366bb37f18098e88
imechaozhang <chaozhang@uchicago.edu>
2020-06-26 14:15:22 -0500

    static root

--- django_survey_app/settings.py
+++ django_survey_app/settings.py
@@ -18,12 +18,13 @@ init()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['meaddjango.azurewebsites.net']
 
